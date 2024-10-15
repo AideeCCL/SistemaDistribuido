6637e1c145ef795e4e23c5558aa89af07d047cfb
AideeCCL <91920359+AideeCCL@users.noreply.github.com>
2024-10-15 09:57:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import socket
 import time
 import threading
 
-from getIP import get_ipv4
+def get_ipv4():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))
+            ip_address = s.getsockname()[0]
+            return ip_address
+    except Exception as e:
+        print("Se ha detectado un error:", e)
+        return None
 
 def main():
     ipv4 = get_ipv4()
